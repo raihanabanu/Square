8550ddf52f1b1fdce1df7f85a669ae6c034e57a4
raihanabanu <raihanabanu.s@gmail.com>
2016-08-11 05:14:53 -0700

    Create Squarepro.java

---
+++ Squarepro.java
@@ -0,0 +1,20 @@
+# Square
+public static Squarepro{
+public stsic void main (String args[]){
+     int j=integer.parseInt(args[0]);
+     int k=integer.parseInt(args[1]);
+     int l=integer.parseInt(args[2]);
+     int m=integer.parseInt(args[3]);
+     int n=integer.parseInt(args[4]);
+     int o=integer.parseInt(args[5]);
+     int p=integer.parseInt(args[6]);
+     int q=integer.parseInt(args[7]);
+     if((arg[0]==args[1])&&(arg[2]==args[3])&&(arg[4]==args[5])&&(arg[6]==args[7])){
+     System.out.println("Square");
+     }
+     else{
+     System.out.println("Not square");
+     }
+     }
+     }
+     
